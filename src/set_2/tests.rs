0f634bcd7dbeee4d5c66ff284b7b7efb32bdbc9b
Faiçal Tchirou <ftchirou@gmail.com>
2015-04-06 16:08:52 +0000

    add test of challenge 9 of set 2

--- src/set_2/tests.rs
+++ src/set_2/tests.rs
@@ -1,1 +1,12 @@
+use util::Bytes;
+
 use super::challenge_9;
+
+#[test]
+fn implement_pkcs7_padding() {
+    let mut input: Bytes = Bytes::from_ascii_string("YELLOW SUBMARINE".to_string());
+
+    challenge_9::pkcs7_pad(&mut input, 20);
+
+    assert_eq!("59454c4c4f57205355424d4152494e4504040404", input.to_hex_string());
+}
